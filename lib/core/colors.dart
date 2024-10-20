545e734b66b1da896b5eb73e3deeea54c77ffeca
qais25 <qaisabdullahbafayadh@gmail.com>
2024-10-21 01:05:32 +0300

    Create : class ColorsApp

---
+++ lib/core/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ColorsApp {
+  static const Color primaryColor = Color.fromARGB(255, 0, 0, 0);
+  static const Color secondaryColor = Color.fromARGB(255, 255, 255, 255);
+}
