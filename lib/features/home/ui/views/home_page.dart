92ae02225ad44ea84f2df92488968386cef808dc
qais25 <qaisabdullahbafayadh@gmail.com>
2024-10-20 23:55:07 +0300

    Create : Home Page

--- lib/features/home/ui/views/home_page.dart
+++ lib/features/home/ui/views/home_page.dart
@@ -2,9 +2,23 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            width: width,
+            height: height * 0.8,
+            decoration: const BoxDecoration(
+                image: DecorationImage(image: AssetImage("assets/logo.png"))),
+          ),
+          const Text("The Only Study App")
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:study_app/core/theme.dart';
 
 import 'features/home/ui/views/home_page.dart';
 
@@ -12,8 +13,10 @@ class StudyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: lightMode,
+      home: const HomePage(),
     );
   }
 }
