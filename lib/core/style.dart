d3f6749b3ca7e1cd9779c39e66e7221b198053de
qais25 <qaisabdullahbafayadh@gmail.com>
2024-10-21 01:06:07 +0300

    Add : abstract class Style

--- lib/core/style.dart
+++ lib/core/style.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
 abstract class Style {
-  final textStyle12 =
+  static TextStyle textStyle12 =
       const TextStyle(fontWeight: FontWeight.w500, fontSize: 12);
+  static TextStyle textStyle14 =
+      const TextStyle(fontWeight: FontWeight.w500, fontSize: 14);
+  static TextStyle textStyle16 =
+      const TextStyle(fontWeight: FontWeight.w500, fontSize: 16);
+  static TextStyle textStyle18 =
+      const TextStyle(fontWeight: FontWeight.w500, fontSize: 18);
+  static TextStyle textStyle28 =
+      const TextStyle(fontWeight: FontWeight.w500, fontSize: 28);
 }
